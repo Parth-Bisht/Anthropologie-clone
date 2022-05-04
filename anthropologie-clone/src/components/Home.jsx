dcfcbad50a15765dc1af72663fbaf805aa1a7f33
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-05-04 17:19:10 +0530

    homepage
    not done....working

---
+++ anthropologie-clone/src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Homemodule.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <div className='upper'>
+            <div className='upleft'>
+                <div className='full'><img src="https://images.ctfassets.net/5de70he6op10/6O4o9CPoov1N3SUAzpVx0J/5a33425eac3c498bc66a2d0aa4795c67/050222_HPG_LS_M1.jpg?w=1756&q=80&fm=webp"/></div>
+                <div className='mid'>
+                    <div className='midimage'><img src="https://images.ctfassets.net/5de70he6op10/2cIhOhx0EenQ1hIURAWLK7/7f322ffde6eb24472f9454e18f457027/050222_HPG_2UP_Dresses.jpg?w=856&q=80&fm=webp"/> </div>
+                    <div className='midimage'><img src="https://images.ctfassets.net/5de70he6op10/j4jfRLSCojibA9tLTz7tq/fe904a67d777bf6af9da224f0e5c227f/050222_HPG_2UP_Sets.jpg?w=856&q=80&fm=webp" /></div>
+                </div>
+                <div className='full'><img src="https://images.ctfassets.net/5de70he6op10/4L4MxwDigk4N40JIiWpXeq/264edf2c0e8af695014b8e0c7ad392dd/050222_HPG_LS_M2.jpg?w=1756&q=80&fm=webp"/></div>
+            </div>
+            <div className='up-right'>
+                <div><img src="https://images.ctfassets.net/5de70he6op10/6FzBOZH3ftSw31v7wApRE9/96d12fc9fedd4ae4154d909c021c75ad/050222_HPG_RR_Furniture.jpg?w=856&q=80&fm=webp"/></div>
+                <div><img src="https://images.ctfassets.net/5de70he6op10/1QeC1AgqnnSUUNUm1L8Cop/26d6959e95bb51ff30016138680010bc/050222_HPG_RR_Candles.jpg?w=856&q=80&fm=webp"/></div>
+                <div><img src="https://images.ctfassets.net/5de70he6op10/3Fp3TqyVqgSjPQZthY3sFD/9386f17ed897837b78eb461bf903c72a/050222_HPG_RR_Swim.jpg?w=856&q=80&fm=webp"/></div>
+                <div><img src="https://images.ctfassets.net/5de70he6op10/5MuarHK2smpFuUB28seiUk/50482d40225937b5dcd47d597990c52d/050222_HPG_RR_Bedding.jpg?w=856&q=80&fm=webp"/></div>
+            </div>
+        </div>
+        <div className='middle'><img src="https://images.ctfassets.net/5de70he6op10/7DEijhrVDU6Yq9fW3H4NYu/1ccd807faed448b9ca75bac79944b9c5/050222_HPG_LS_M3.jpg?w=2694&q=80&fm=webp"/></div>
+    </div>
+  )
+}
+
+export default Home
