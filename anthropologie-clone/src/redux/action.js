2fb296406f3681ba6ad0fd946d1e15a158a8e616
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-06-01 13:38:19 +0530

    Anthropologie
    Anthropologie

--- anthropologie-clone/src/redux/action.js
+++ anthropologie-clone/src/redux/action.js
@@ -3,8 +3,8 @@ import { LOGIN_FAILURE, LOGIN_SUCCESS } from "./actionTypes"
 export const loginSuccess = (payload)=>({
     type: LOGIN_SUCCESS,
     payload
-})
+});
 
 export const loginFailure = ()=>({
     type: LOGIN_FAILURE
-})
+});
